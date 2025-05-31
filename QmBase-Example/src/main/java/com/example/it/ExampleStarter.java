6cb0e80d49bd3a369732a302f6e50aa69d9c7c41
GradyJean <grady.jean.king@gmail.com>
2025-05-31 14:06:42 +0800

    增加示例项目

---
+++ QmBase-Example/src/main/java/com/example/it/ExampleStarter.java
@@ -0,0 +1,11 @@
+package com.example.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleStarter.class, args);
+    }
+}
---
+++ QmBase-Example/src/main/java/com/example/it/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.example.it.controller;
+
+import com.qm.base.shared.base.model.Result;
+import com.qm.base.shared.logger.annotation.Log;
+import com.qm.base.shared.logger.core.QmLog;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+public class TestController {
+    @Log
+    @RequestMapping("test")
+    public Result<String> test() {
+        QmLog.info("test");
+        log.info("test");
+        return Result.SUCCESS("index");
+    }
+}
